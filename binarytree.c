39111cfac62c4c2c796c1387b250a3ce2bdf471e
nicholas-shepard <shepley1322@hotmail.com>
2019-03-07 17:12:52 -0700

    Added blank function definitions, did some initial work in the Initialize and Search functions

--- binarytree.c
+++ binarytree.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "binarytree.h"
 
 void initialize(binary_tree* bt) {
-
+	bt->size = 0;
+	bt->root = NULL;
 }
 
 bool search(binary_tree* bt, int key) {
+	node* tmpNode = bt->root;
+	if (key == tmpNode->data) {
+		return True
+	}
 
+	else if (key < tmpNode->data) {
+		tmpNode = tmpNode->left;
+	}
+	
+	else {
+		tmpNode = tmpNode->right;
+	}
 }
 
 void insert(binary_tree* bt, int item) {
