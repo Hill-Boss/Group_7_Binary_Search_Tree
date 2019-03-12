4e3568e58a88375f786bed42962b9727ca65ab19
Jacob Bexten <jacob.bexten@umontana.edu>
2019-03-12 15:13:33 -0600

    added nodepostorder

--- binarytree.h
+++ binarytree.h
@@ -15,6 +15,7 @@ void printinorder(binary_tree* bt);
 void nodeinorder(node* node);
 void printpreorder(binary_tree* bt);
 void printpostorder(binary_tree* bt);
+void nodepostorder(node* node);
 int btsize(binary_tree* bt);
 int treeheight(binary_tree* bt);
 
