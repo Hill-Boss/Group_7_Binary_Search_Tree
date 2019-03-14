d743ab3fdae7c914ae99b5bc6f6762b3ae0670f5
Jacob Bexten <jacob.bexten@umontana.edu>
2019-03-14 14:13:39 -0600

    commenting

--- binarytree.c
+++ binarytree.c
@@ -92,6 +92,7 @@ void printpreorder(binary_tree* bt) {
 }
 
 void nodepostorder(node* node) {
+	// if node not null, print node->left and node->right. Then prints node->data
 	if (node != NULL) {
 		nodepostorder(node->left);
 		nodepostorder(node->right);
@@ -100,6 +101,7 @@ void nodepostorder(node* node) {
 }
 
 void printpostorder(binary_tree* bt) {
+	//passes bt->root into nodepostorder
 	nodepostorder(bt->root);
 }
 
